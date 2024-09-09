c45c925af1c77304972f179f7b616844c6ec02d5
beekebak <ribehadance@gmail.com>
2024-09-09 16:07:56 +0700

    task1 done

---
+++ lab1/task1/check_maps_thread.c
@@ -0,0 +1,46 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int global = 0;
+
+void *mythread(void *arg) {
+	int local = 0;
+	static int local_static = 0;
+	const int local_const = 0;
+	sleep(3);
+	printf("mythread [%d %d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid(), pthread_self());
+	global += 1;
+	local += 1;
+	printf("global %d, local %d\n", global, local);
+	printf("global %d, local %d, local const %d, local static %d\n", &global, &local, &local_const, &local_static);
+        return NULL;
+}
+
+int main() {
+	pthread_t tid;
+	int err;
+
+	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+	sleep(10);
+
+        for(int i = 0; i < 5; i++) {
+		err = pthread_create(&tid, NULL, mythread, NULL);
+		if (err) {
+	    	printf("main: pthread_create() failed: %s\n", strerror(err));
+			return -1;
+		} else {
+		    	printf("main: pthread created with tid %d\n", tid);
+		}
+		pthread_detach(tid);
+        }
+	sleep(10);
+	pthread_exit(NULL);
+	return 0;
+}
+
