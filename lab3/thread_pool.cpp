dce12e5b93a7b64f6b95d6b142b41b7207caa37f
b.kovalev <ribehadance@gmail.com>
2024-12-08 06:49:14 +0700

    thread pool probably

---
+++ lab3/thread_pool.cpp
@@ -0,0 +1,35 @@
+#include "thread_pool.hpp"
+
+void* thread_func(void* arg){
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    proxy_server_t* server = static_cast<proxy_server_t*>(arg);
+    server->start_server_loop();
+    std::cout << "thread finished " << pthread_self() << "\n";
+    return NULL;
+}
+
+
+thread_pool_t::thread_pool_t(size_t num_threads): num_threads(num_threads), next(0), 
+        thread_ids(num_threads), proxy_servers(num_threads) {  
+    for (size_t i = 0; i < num_threads; ++i){
+        int res = pthread_create(thread_ids[0+i], NULL, thread_func, proxy_servers[0+i]);
+        if (res != 0){
+            errno = res;
+            perror("init thread_poll");
+            abort();
+        }
+    }
+}
+
+thread_pool_t::~thread_pool_t(){
+    for (size_t i = 0; i < num_threads; ++i){
+        pthread_cancel(*thread_ids[i]);
+        pthread_join(*thread_ids[i], NULL);
+    }
+}
+
+
+void thread_pool_t::add_new_connection(int fd){
+    proxy_servers[next]->add_client_socket(fd);
+    next = (next + 1) % num_threads;
+}
